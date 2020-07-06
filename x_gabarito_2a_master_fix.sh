a555cd1eddb7e9b3bd0c6745a3c76c71362fb8d6
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-07-06 12:21:36 -0300

    Update x_gabarito_2a_master_fix.sh

--- x_gabarito_2a_master_fix.sh
+++ x_gabarito_2a_master_fix.sh
@@ -1,7 +1,11 @@
 #!bash
 
 # corrigir a configuração do ARQUIVO ./kube/config para a PORTA 6443
-
+cat ./.kube/config | grep server
+# ex:    server: https://172.17.0.66:2379
+echo 'Digite o IP que aparece acima (ex: 172.17.0.66)'
+read IP_CLUSTER
+sed -i 's|server: https:.*:2379|server: https:'$IP_CLUSTER':6443|' /root/.kube/config
 
 # corrigir a configuração do API SERVER:
 
